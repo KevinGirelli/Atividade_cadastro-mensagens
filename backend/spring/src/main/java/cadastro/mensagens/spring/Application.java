593e2f70c27752ef2f4ae1705edcb8935087f751
Kevin Girelli <kevingirelli.gg@gmail.com>
2024-04-30 14:13:31 -0300

    add Spring and Angular

---
+++ backend/spring/src/main/java/cadastro/mensagens/spring/Application.java
@@ -0,0 +1,13 @@
+package cadastro.mensagens.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
