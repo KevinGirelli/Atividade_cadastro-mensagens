f71bad01bd4372ed14cc49e099a756c6d113ef90
Kevin Girelli <kevingirelli.gg@gmail.com>
2024-05-12 14:44:39 -0300

    Add Home Page

--- frontend/angular_files/src/app/app.routes.ts
+++ frontend/angular_files/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { EnviarMensagemComponent } from './components/enviar-mensagem/enviar-mensagem.component';
 import { ListarMensagemComponent } from './components/listar-mensagem/listar-mensagem.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
   {
     path: "enviar",
     component: EnviarMensagemComponent
